34ab332ed6456662f4286b46f0406e6e43534896
Marcelo Mergulhão <mmergulhao@daitangroup.com>
2018-07-11 11:12:06 -0300

    Use default 404 page

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,3 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 from app import views
-
-# Sample HTTP error handling
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html'), 404
