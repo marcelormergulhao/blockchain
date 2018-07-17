af8dcf5b1d88b038a0ddfaff804697cadd4e39df
Marcelo Mergulhão <mmergulhao@daitangroup.com>
2018-07-17 13:05:26 -0300

    Refactor code to separate P2P and blockchain functionality

--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-import hashlib
 import random
 import json
 import requests
@@ -12,6 +11,8 @@ from Crypto.Signature import PKCS1_v1_5
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
 from base64 import b64encode, b64decode
+from collections import OrderedDict
+from threading import Lock
 
 # Log configuration
 logging.basicConfig(format = "%(asctime)-15s %(message)s", stream=sys.stdout)
@@ -31,7 +32,9 @@ class Transaction():
         self.signature = b64encode(signer.sign(digest)).decode()
 
     def get_json(self):
-        return {"addr_from": self.addr_from, "addr_to": self.addr_to}
+        ordered_json = OrderedDict({"addr_from": self.addr_from})
+        ordered_json["addr_to"] = self.addr_to
+        return ordered_json
 
     def get_signed_json(self, key):
         if self.signature is None:
@@ -39,20 +42,21 @@ class Transaction():
 
         # Use this append "==" trick to avoid incorrect padding
         pubkey = b64encode(key.publickey().exportKey("PEM") + b"==").decode()
-        return {"addr_from": self.addr_from, "addr_to": self.addr_to,
-                "signature": self.signature, "pubkey": pubkey}
+        ordered_json = self.get_json()
+        ordered_json["signature"] = self.signature
+        ordered_json["pubkey"] = pubkey
+        return ordered_json
         
 
 class Block():
     def __init__(self, prevHash, height, data, miner):
-        self.block = {
-            "miner": miner,
-            "hash": "",
-            "prevHash": prevHash,
-            "height": height,
-            "nonce": 0, # Nonce will be used to "mine" node
-            "data": list(data)
-        }
+        self.block = OrderedDict({"miner": miner})
+        self.block["hash"] = ""
+        self.block["prevHash"] = prevHash
+        self.block["height"] = height
+        # Nonce will be used to "mine" node
+        self.block["nonce"] = 0
+        self.block["data"] = list(data)
 
     def mine(self):
         """Calculate valid hash from current transaction list"""
@@ -64,37 +68,190 @@ class Block():
             hexdigest = sha256.hexdigest()
             if hexdigest[0:3] == "000":
                 self.block["hash"] = hexdigest
+                self.block["timestamp"] = str(datetime.now().timestamp())
                 break
             self.block["nonce"] = self.block["nonce"] + 1
 
     def get_json(self):
         return self.block
 
-
-
 class Blockchain():
     def __init__(self):
         """
-        Blockchain initialization routine, generates miner ID and synchronizes blockchain (blocks and participants)
+        Initialize blockchain storage and possibly populate
+        """
+        self.lock = Lock()
+        self.t_lock = Lock()
+        # self.conflicting_block = None
+        self.storage = []
+        self.transaction_pool = []
+
+    def empty(self):
+        return len(self.storage) == 0
+
+    def setup_new_chain(self, json_list):
+        self.storage = list(json_list)
+    
+    def create_genesis_block(self, private_key, miner_id):
+        logger.info("Creating genesis block")
+        transaction = Transaction("Genesis Addr", "Genesis Block")
+        genesis = Block("Genesis Block", 0, [transaction.get_signed_json(private_key)], miner_id)
+        genesis.mine()
+        self.storage = [genesis.get_json()]
+
+    def check_transaction(self, miner_id):
+        for block in self.storage:
+            logger.info("Block {}".format(block))
+            transactions = block["data"]
+            for transaction in transactions:
+                logger.info("Checking transaction {}".format(transaction))
+                if transaction["addr_from"] == miner_id:
+                    logger.error("User has already issued his vote")
+                    return True
+        # Also check for soon to be completed transactions
+        for transaction in self.transaction_pool:
+            if transaction["addr_from"] == miner_id:
+                logger.error("User has vote on transaction pool")
+        return False
+
+    def remove_transactions_from_pool(self, block):
+        """
+        After receiveing a new block, check if the transactions were in the pool and remove them
+        """
+        logger.info("Current pool {}".format(self.transaction_pool))
+        for transaction in block["data"]:
+            if transaction in self.transaction_pool:
+                self.transaction_pool.remove(transaction)
+        logger.info("New pool {}".format(self.transaction_pool))
+        return
+
+    def validate_block(self, block, prevBlock):
+        logger.info("Validate block")
+        if prevBlock is not None:
+            # Validate consistence with blockchain
+            if block["prevHash"] == prevBlock["hash"]:
+                logger.info("Block is consistent with blockchain")
+                # Validate PoW
+                if block["hash"][0:3] == "000":
+                    logger.info("Block has PoW")
+                    # Validate the block hash is from itself, create same block structure
+                    transactions = []
+                    for t in block["data"]:
+                        new_t = OrderedDict({"addr_from": t["addr_from"]})
+                        new_t["addr_to"] = t["addr_to"]
+                        new_t["signature"] = t["signature"]
+                        new_t["pubkey"] = t["pubkey"]
+                        transactions.append(new_t)
+                        
+                    striped_block = Block(block["prevHash"], block["height"], transactions, block["miner"])
+                    striped_block.block["nonce"] = block["nonce"]
+                    striped_block.block["hash"] = ""
+                    logger.info("Striped {}".format(striped_block.get_json()))
+                    sha256 = SHA256.new()
+                    sha256.update(json.dumps(striped_block.get_json()).encode())
+                    hexdigest = sha256.hexdigest()
+                    if hexdigest == block["hash"]:
+                        logger.info("Block generates the hash provided")
+                        for transaction in block["data"]:
+                            if not self.validate_transaction(transaction):
+                                return False
+                        logger.info("Block has all transactions valid")
+                        logger.info("Accept block")
+                        return True
+                    else:
+                        logger.info("Invalid block hash")
+        return False
+
+    def create_and_add_block(self, miner_id):
+        block = None
+        if not self.empty():
+            # Get last block
+            prevBlock = self.storage[-1]
+            # Use it to create new block with the current transactions in pool
+            block = Block(prevBlock["hash"], prevBlock["height"] + 1, self.transaction_pool, miner_id)
+            block.mine()
+            logger.info("Block created: {}".format(block.get_json()))
+            self.validate_and_add_block(block.get_json())
+        return block
+
+    def validate_transaction(self, transaction):
+        t_sig = b64decode(transaction["signature"].encode())
+        pubkey = b64decode(transaction["pubkey"].encode())
+        verifier = PKCS1_v1_5.new(RSA.importKey(pubkey))
+        digest = SHA256.new()
+        t = Transaction(transaction["addr_from"], transaction["addr_to"])
+        digest.update(str(t.get_json()).encode())
+        verified = verifier.verify(digest,t_sig)
+        if verified:
+            return True
+        else:
+            logger.info("Signature is invalid")
+        return False
+
+    def add_transaction_to_pool(self, transaction):
+        self.t_lock.acquire()
+        self.transaction_pool.append(transaction)
+        self.t_lock.release()
+        return
+
+    def validate_and_add_block(self, block):
+        logger.info("Validate and add block")
+        if len(self.storage) > 0:
+            # logger.info("Block received: {}".format(block))
+            self.lock.acquire()
+            current_head = self.storage[-1]
+            # Check if node has the same height as the current head and untie the conflict with the timestamp
+            if current_head["height"] == block["height"]:
+                logger.info("Validate conflicting block")
+                prevBlock = self.storage[-2]
+                if current_head["timestamp"] > block["timestamp"]:
+                    if self.validate_block(block, prevBlock):
+                        # Recover transactions that do not match
+                        for transaction in current_head["data"]:
+                            if transaction not in block["data"]:
+                                self.add_transaction_to_pool(transaction)
+                        self.storage[-1] = block
+                elif current_head["timestamp"] == block["timestamp"]:
+                    # Untie timestamp conflict with miner id
+                    if current_head["miner"] > block["miner"]:
+                        if self.validate_block(block, prevBlock):
+                            # Recover transactions that do not match
+                            for transaction in current_head["data"]:
+                                if transaction not in block["data"]:
+                                    self.add_transaction_to_pool(transaction)
+                            self.storage[-1] = block
+            # Otherwise, the node just must be valid
+            elif current_head["height"] == block["height"] - 1:
+                logger.info("Validate next block")
+                if self.validate_block(block, current_head):
+                    self.storage.append(block)
+                    self.remove_transactions_from_pool(block)
+            self.lock.release()
+        return False
+
+    def get_chain(self):
+        return self.storage
+
+class PeerToPeer():
+    def __init__(self, addr):
+        """
+        PeerToPeer network initialization routine, generates miner ID and synchronizes blockchain (blocks and participants)
         """
         self.master_node = "localhost:5000"
         random.seed()
         self.generate_miner_id()
-        self.address = self.get_current_address()
+        self.address = addr
         self.participant_list = []
         self.get_current_participant_list()
         self.advertise()
-        self.blockchain = None
+        self.blockchain = Blockchain()
 
-        while self.blockchain is None:
+        while self.blockchain.empty():
             self.get_current_blockchain()
         # Valid addresses you can issue a vote to
         self.valid_addresses = [{"name": "Candidate 1", "address": "12345"},
                                 {"name": "Candidate 2", "address": "5678"},
                                 {"name": "Candidate 3", "address": "9999"}]
-        self.transaction_pool = []
-        # Control the token for "current block creator"
-        self.update_block_creator()
         self.sched = BackgroundScheduler(daemon=True)
         self.sched.start()
 
@@ -148,12 +305,11 @@ class Blockchain():
                 r = requests.get("http://" + self.participant_list[random_participant]["address"] + "/blockchain")
                 logger.debug("Request result: {}".format(r.status_code))
                 if r.status_code == 200:
-                    self.blockchain = r.json()
-                    logger.debug("Current Blockchain {}".format(self.blockchain))
+                    self.blockchain.setup_new_chain(r.json())
             else:
                 logger.info("Current node is the only one in the participant list")
-                if self.blockchain is None:
-                    self.create_genesis_block()
+                if self.blockchain.empty():
+                    self.blockchain.create_genesis_block(self.private_key, self.miner_id)
         else:
             # There should never be an empty participant list
             logger.error("Empty participant list, won't get blockchain for now")        
@@ -171,7 +327,7 @@ class Blockchain():
             # There should never be an empty participant list
             logger.error("Empty participant list, won't get transaction pool for now")
 
-    def add_transaction_to_pool(self, addr_to):
+    def create_and_add_transaction(self, addr_to):
         """
         Used to make a transaction from current node to another
         """
@@ -179,12 +335,11 @@ class Blockchain():
         if self.has_to_vote():
             if self.check_valid_address(addr_to):
                 transaction = Transaction(self.miner_id, addr_to)
-                self.transaction_pool.append(transaction.get_signed_json(self.private_key))
+                self.blockchain.add_transaction_to_pool(transaction.get_signed_json(self.private_key))
                 self.propagate_transaction(transaction.get_signed_json(self.private_key))
-                if self.block_creator:
-                    if not self.sched.get_jobs():
-                        logger.info("Start block schedule")
-                        self.sched.add_job(self.create_and_add_block, 'date',run_date=datetime.now()+timedelta(seconds=10))
+                if len(self.sched.get_jobs()) == 0:
+                    logger.info("Start block schedule")
+                    self.sched.add_job(self.create_and_add_block, 'date',run_date=datetime.now()+timedelta(seconds=5))
             else:
                 logger.error("Cannot vote for this ledger, check the address")
 
@@ -193,22 +348,10 @@ class Blockchain():
         Check if node still hasn't voted
         """
         logger.info("Checking blockchain for node votes")
-        if self.blockchain is not None:
-            for block in self.blockchain:
-                logger.info("Block {}".format(block))
-                transactions = block["data"]
-                for transaction in transactions:
-                    logger.info("Checking transaction {}".format(transaction))
-                    if transaction["addr_from"] == self.miner_id:
-                        logger.error("User has already issued his vote")
-                        return False
-        # Also check for soon to be completed transactions
-        for transaction in self.transaction_pool:
-            if transaction["addr_from"] == self.miner_id:
-                logger.error("User has vote on transaction pool")
+        if not self.blockchain.empty():
+            if self.blockchain.check_transaction(self.miner_id):
                 return False
         return True
-
 
     def check_valid_address(self, address):
         logger.info("Check destination address")
@@ -217,43 +360,13 @@ class Blockchain():
                 return True
         return False
 
-    def create_genesis_block(self):
-        logger.info("Creating genesis block")
-        transaction = Transaction("Genesis Addr", "Genesis Block")
-        genesis = Block("Genesis Block", 0, [transaction.get_signed_json(self.private_key)], self.miner_id)
-        genesis.mine()
-        self.blockchain = [genesis.get_json()]
-
     def create_and_add_block(self):
         """
         Create block using current transaction pool as data
         """
-        block = None
-        if self.blockchain is not None:
-            prevBlock = self.blockchain[-1]
-            block = Block(prevBlock["hash"], prevBlock["height"] + 1, self.transaction_pool, self.miner_id)
-            block.mine()
-            logger.info("Block created: {}".format(block.get_json()))
-            # After adding current transaction to block, remove transactions from pool
-            self.blockchain.append(block.get_json())
-            self.remove_transactions_from_pool(block.get_json())
-            self.propagate_block(block.get_json())
-        return block
-
-    def remove_transactions_from_pool(self, block):
-        """
-        After receiveing a new block, check if the transactions were in the pool and remove them
-        """
-        logger.info("Current pool {}".format(self.transaction_pool))
-        for transaction in block["data"]:
-            if transaction in self.transaction_pool:
-                self.transaction_pool.remove(transaction)
-        logger.info("New pool {}".format(self.transaction_pool))
+        block = self.blockchain.create_and_add_block(self.miner_id)
+        self.propagate_block(block.get_json())
         return
-
-    def show_blockchain(self):
-        for block in self.blockchain:
-            print(block)
 
     def generate_miner_id(self):
         """
@@ -298,51 +411,28 @@ class Blockchain():
                 if r.status_code == 200:
                     logger.info("Sent block to {}".format(peer["address"]))
 
-    def update_block_creator(self):
-        """
-        Check if it is this node time to generate blocks
-        """
-        self.block_creator = True
 
     def validate_and_add_block(self, block):
-        # Check if received block fits the position of new block
-        if self.blockchain is not None:
-            logger.info("Block received: {}".format(block))
-            prevBlock = self.blockchain[-1]
-            if block["prevHash"] == prevBlock["hash"]:
-                if block["hash"][0:3] == "000":
-                    logger.info("Valid block")
-                    self.blockchain.append(block)
-                    self.remove_transactions_from_pool(block)
-                    self.propagate_block(block)
+        self.blockchain.validate_and_add_block(block)
         return
 
     def validate_and_add_transaction(self, transaction):
         logger.info("Transaction received: {}".format(transaction))
         # First check if the signature is ok
-        t_sig = b64decode(transaction["signature"].encode())
-        pubkey = b64decode(transaction["pubkey"].encode())
-        verifier = PKCS1_v1_5.new(RSA.importKey(pubkey))
-        digest = SHA256.new()
-        t = Transaction(transaction["addr_from"], transaction["addr_to"])
-        digest.update(str(t.get_json()).encode())
-        verified = verifier.verify(digest,t_sig)
-        if verified:
+        if self.blockchain.validate_transaction(transaction):
+            # Then check the destination address
             logger.info("Verified signature")
             for valid_addr in self.valid_addresses:
                 if transaction["addr_to"] == valid_addr["address"]:
-                    self.transaction_pool.append(transaction)
-                    break
-        else:
-            logger.info("Signature is invalid")
+                    self.blockchain.add_transaction_to_pool(transaction)
+                    if len(self.sched.get_jobs()) == 0:
+                        logger.info("Start block schedule")
+                        self.sched.add_job(self.create_and_add_block, 'date',run_date=datetime.now()+timedelta(seconds=5))
         return
 
-
-    def get_current_address(self):
-        return "localhost:5000"
-
     def add_participant_to_list(self, peer):
-        self.participant_list.append(peer)
+        if peer not in self.participant_list:
+            self.participant_list.append(peer)
 
     def advertise(self):
         logger.info("Advertise node to other peers")
@@ -357,4 +447,4 @@ class Blockchain():
         # Add current node to its own list
         self.participant_list.append({"miner_id":self.miner_id, "address": self.address})
             
-
+        
--- app/views.py
+++ app/views.py
@@ -1,38 +1,38 @@
 from flask import request, jsonify, render_template, redirect
 from app import app
-from app.models import Blockchain,Block
+from app.models import Blockchain,Block,PeerToPeer
 
-blockchain = Blockchain()
+network = PeerToPeer("localhost:5000")
 
 @app.route("/")
 @app.route("/status")
 def index():
-    return render_template("status.html", blockchain=blockchain)
+    return render_template("status.html", blockchain=network)
 
 @app.route("/cast_vote", methods=["POST"])
 def cast_vote():
     if request.form is not None:
         print("Cast vote to {}".format(request.form["vote_addr"]))
-        blockchain.add_transaction_to_pool(request.form["vote_addr"])
+        network.create_and_add_transaction(request.form["vote_addr"])
     return redirect("/status")
 
 @app.route("/list")
 def get_miner_list():
     """Return list of miners advertised to this node"""
-    return jsonify(blockchain.participant_list)
+    return jsonify(network.participant_list)
 
 @app.route("/advertise", methods=["POST"])
 def advertise():
     """Receive node advertisement and store on miner list"""
     received_data = request.get_json()
     if received_data is not None:
-        blockchain.add_participant_to_list(received_data)
+        network.add_participant_to_list(received_data)
     return jsonify({"status": "ok"})
 
 @app.route("/blockchain")
 def get_blockchain():
     """Return current blockchain"""
-    return jsonify(blockchain.blockchain)
+    return jsonify(network.blockchain.get_chain())
 
 @app.route("/update_pool", methods=["POST"])
 def add_transaction():
@@ -40,7 +40,7 @@ def add_transaction():
     received_data = request.get_json()
     if received_data is not None:
         print("Received Pool {}".format(received_data))
-        blockchain.validate_and_add_transaction(received_data)
+        network.validate_and_add_transaction(received_data)
     return redirect("/status")
 
 @app.route("/add_new_block", methods=["POST"])
@@ -49,5 +49,5 @@ def add_block():
     received_data = request.get_json()
     if request.json is not None:
         print("Received Block {}".format(received_data))
-        blockchain.validate_and_add_block(received_data)
+        network.validate_and_add_block(received_data)
     return jsonify({"status": "ok"})
